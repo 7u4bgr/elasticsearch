79cfaa4ef96210a92bf01dd3691d6aa47936917b
7u4bgr <abdllyevv@gmail.com>
2025-04-08 15:55:56 +0300

    Elasticsearch with Kibana and Docker

---
+++ src/main/java/com/example/elasticsearchlearn/ElasticsearchlearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchlearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elasticsearchlearn/ElasticsearchlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
